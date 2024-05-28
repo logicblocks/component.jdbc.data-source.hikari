045df979a8ccbac1afe80b043d2b3e5dab9e447a
Circle CI <circleci@logicblocks.io>
2024-05-28 13:58:13 +0000

    Pre-release version 0.1.3-RC5 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/component.jdbc.data-source.hikari "0.1.3-RC4"
+(defproject io.logicblocks/component.jdbc.data-source.hikari "0.1.3-RC5"
   :description "A component providing a HikariCP JDBC data source."
   :url "https://github.com/logicblocks/component.jdbc.data-source.hikari"
 
